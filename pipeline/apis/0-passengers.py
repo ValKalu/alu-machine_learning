c11ba2d31bad20a5d332408443e4fcd3a75eaf16
ValKalu <kalu.valentine01@gmail.com>
2024-06-27 11:24:53 +0200

    comments

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to fetch available starships that can hold a given number of passengers from the Swapi API.
+"""
+
 import requests
 def availableShips(passangerCOunt):
+    """
+    Fetches a list of ships that can hold a given number of passengers from the Swapi API.
+
+    Args:
+        passengerCount (int): The minimum number of passengers the ships should be able to hold.
+
+    Returns:
+        list: A list of ship names that can hold the given number of passengers.
+    """
     url = 'https://swapi.dev/api/starships/'
     ships = []
     
