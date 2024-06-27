212ad0120382222ca83ba536dfcbe0d7423ced8c
ValKalu <kalu.valentine01@gmail.com>
2024-06-28 01:14:39 +0200

    bug

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -17,31 +17,24 @@ def sentientPlanets():
     """
     url = 'https://swapi.dev/api/species/'
     planets = []
-    planets_not_found = []
 
     while url:
         response = requests.get(url)
         data = response.json()
-
         for species in data['results']:
-            if species.get('designation') == 'sentient' and species.get('homeworld'):
+            if species['designation'] == 'sentient' and species['homeworld']:
                 homeworld_response = requests.get(species['homeworld'])
                 if homeworld_response.status_code == 200:
                     homeworld_data = homeworld_response.json()
                     planets.append(homeworld_data['name'])
                 else:
-                    planets_not_found.append(species['name'])
-            elif species.get('designation') == 'sentient' and not species.get('homeworld'):
-                planets_not_found.append(species['name'])
+                    planets.append('unknown')
+        url = data['next']
 
-        url = data.get('next')
-
-    return planets, planets_not_found
+    return planets
 
 
 if __name__ == '__main__':
-    planets, planets_not_found = sentientPlanets()
-    if planets_not_found:
-        print(f"Planets not found: {planets_not_found}")
+    planets = sentientPlanets()
     for planet in planets:
         print(planet)
