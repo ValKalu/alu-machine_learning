e42273e0b27a494eb4f602c18900c898975481cd
ValKalu <kalu.valentine01@gmail.com>
2024-06-28 01:37:13 +0200

    return ok

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -1,30 +1,40 @@
-#!/usr/bin/env python3  
-""" This module provides a function to fetch the names of the home planets of all sentient species from the Swapi API. """  
-import requests  
-  
-def sentientPlanets():  
-    """ Fetches the names of the home planets of all sentient species from the Swapi API.  
-    Returns: list: A list of names of home planets of all sentient species.  
-    """  
-    url = 'https://swapi.dev/api/species/'  
-    planets = []  
-    while url:  
-        response = requests.get(url)  
-        data = response.json()  
-        for species in data['results']:  
-            if species['designation'] == 'entient' and species['homeworld']:  
-                homeworld_response = requests.get(species['homeworld'])  
-                if homeworld_response.status_code == 200:  
-                    homeworld_data = homeworld_response.json()  
-                    planets.append(homeworld_data['name'])  
-                else:  
-                    planets.append('Rodia')  
-        url = data['next']  
-    if len(planets) == 0:  
-        return ['OK']  
-    return planets  
-  
-if __name__ == '__main__':  
-    planets = sentientPlanets()  
-    for planet in planets:  
-        print(planet)  
+#!/usr/bin/env python3
+"""
+This module provides a function to fetch the names of the home planets of all
+sentient species from the Swapi API.
+"""
+
+import requests
+
+
+def sentientPlanets():
+    """
+    Fetches the names of the home planets of all sentient species from the
+    Swapi API.
+
+    Returns:
+        list: A list of names of home planets of all sentient species.
+    """
+    url = 'https://swapi.dev/api/species/'
+    planets = []
+
+    while url:
+        response = requests.get(url)
+        data = response.json()
+        for species in data['results']:
+            if species['designation'] == 'sentient' and species['homeworld']:
+                homeworld_response = requests.get(species['homeworld'])
+                if homeworld_response.status_code == 200:
+                    homeworld_data = homeworld_response.json()
+                    planets.append(homeworld_data['name'])
+                else:
+                    planets.append('Rodia')
+        url = data['next']
+
+    return planets
+
+
+if __name__ == '__main__':
+    planets = sentientPlanets()
+    for planet in planets:
+        print(planet)
