e42e3ff133fe1dbdeafcb7ea9f5c3ffe75a8b1a6
ValKalu <kalu.valentine01@gmail.com>
2024-06-29 15:26:54 +0200

    sentientplanets

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -7,6 +7,7 @@ sentient species from the Swapi API.
 import requests
 
 
+
 def sentientPlanets():
     """
     Fetches the names of the home planets of all sentient species from the
@@ -15,26 +16,24 @@ def sentientPlanets():
     Returns:
         list: A list of names of home planets of all sentient species.
     """
-    url = 'https://swapi.dev/api/species/'
-    planets = []
+    url = "https://swapi-api.alx-tools.com/api/species/"
+    sentient_planets = []
+    sentient_classifications = ["sentient"]
 
     while url:
         response = requests.get(url)
         data = response.json()
-        for species in data['results']:
-            if species['designation'] == 'sentient' and species['homeworld']:
-                homeworld_response = requests.get(species['homeworld'])
-                if homeworld_response.status_code == 200:
-                    homeworld_data = homeworld_response.json()
-                    planets.append(homeworld_data['name'])
-                else:
-                    planets.append('Rodia')
-        url = data['next']
 
-    return planets
+        for species in data["results"]:
+            if (
+                species["classification"].lower() in sentient_classifications
+                and species["homeworld"]
+            ):
+                homeworld_url = species["homeworld"]
+                homeworld_response = requests.get(homeworld_url)
+                homeworld_data = homeworld_response.json()
+                sentient_planets.append(homeworld_data["name"])
 
+        url = data["next"]
 
-if __name__ == '__main__':
-    planets = sentientPlanets()
-    for planet in planets:
-        print(planet)
+    return sentient_planets
