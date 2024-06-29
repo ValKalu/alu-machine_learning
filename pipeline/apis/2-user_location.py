a007ed7229e0e07ab5bffb5ebefa4b06452bceee
ValKalu <kalu.valentine01@gmail.com>
2024-06-29 15:10:03 +0200

    location

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,41 +1,41 @@
 #!/usr/bin/env python3
 """
-This script fetches and prints the location of a specific GitHub user
+    script that prints the location of a specific user:
 """
 
 
 import requests
 import time
-import datetime import datetime
+from datetime import datetime
 
 
 def main(url):
     """
-    Fetches the location of a specific GitHub user.
+    - The user is passed as first argument of the script
+    with the full API URL, example: ./2-user_location.py
+    https://api.github.com/users/holbertonschool
+    - If the user doesn’t exist, print Not found
+    - If the status code is 403, print Reset in X min where X
+    is the number of minutes from now and the value of
+    X-Ratelimit-Reset
+    - Your code should not be executed when the file is
+    imported (you should use if __name__ == '__main__':)
 
-    Args:
-        url (str): The full API URL of the user.
-
-
-
-    Returns:
-        str: The location of the user or an appropriate message if not
-        found or rate limited.
     """
     response = requests.get(url)
 
     if response.status_code == 404:
         print("Not found")
     elif response.status_code == 403:
-        reset_timestamp = int(response.headers["X-RateLimit-Reset"])
+        reset_timestamp = int(response.headers["X-Ratelimit-Reset"])
         current_timestamp = int(time.time())
         reset_in_minutes = (reset_timestamp - current_timestamp) // 60
         print("Reset in {} min".format(reset_in_minutes))
     else:
         print(response.json()["location"])
-        
-        
-        if __name__ == "__main__":
-            import sys
-            
-            main(sys.argv[1])
+
+
+if __name__ == "__main__":
+    import sys
+
+    main(sys.argv[1])
