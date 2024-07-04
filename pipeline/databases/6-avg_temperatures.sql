e67b9addcf7fbac58ae2b595f16626eec8f7c048
ValKalu <kalu.valentine01@gmail.com>
2024-07-05 00:32:47 +0200

    temps

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,6 +1,5 @@
--- Import hbtn_0c_0 database table temperatures.sql
 -- Calculate average temperature (Fahrenheit) by city, ordered by temperature (descending)
-SELECT city, AVG(temperature) AS average_temperature_fahrenheit
+SELECT city, AVG(avg_temp) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY average_temperature_fahrenheit DESC;
+ORDER BY avg_temp DESC;
