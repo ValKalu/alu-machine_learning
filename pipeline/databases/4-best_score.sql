b55a360ac7f5d9b762bf98dad7bb13d344793d27
ValKalu <kalu.valentine01@gmail.com>
2024-07-05 00:08:41 +0200

    create and Insert

--- pipeline/databases/4-best_score.sql
+++ pipeline/databases/4-best_score.sql
@@ -1,11 +1,4 @@
--- Create table and insert data to second_table
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
-
-INSERT INTO second_table (id, name, score) VALUES (1, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUES (2, 'Roy', 3);
-INSERT INTO second_table (id, name, score) VALUES (3, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUES (4, 'Bryan', 8);
+-- List specific records with scores and names
+SELECT score, name
+FROM your_table_name
+WHERE score IN (89, 23, 12);
