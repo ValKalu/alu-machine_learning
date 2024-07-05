c281f2168add684a9b3d267ba50edade51021e19
ValKalu <kalu.valentine01@gmail.com>
2024-07-05 14:05:13 +0200

    max temp

--- pipeline/databases/7-max_state.sql
+++ pipeline/databases/7-max_state.sql
@@ -1,5 +1,5 @@
 ---A Script that displays max tempreature of each state
-SELECT state, max(temperature) as max_temp
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state;
