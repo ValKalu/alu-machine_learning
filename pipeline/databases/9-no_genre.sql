38b820d588c9d189ee6fedd9a23df0cfc68d1765
ValKalu <kalu.valentine01@gmail.com>
2024-07-05 17:10:52 +0200

    accending order lis

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -1,6 +1,6 @@
--- Script that list all shows as tv_show,title-tv_show.genre_id
-SELECT tv_shows.title, tvshows_genres.genres_id
-FROM hbtn_0d_tv_shows
-LEFT JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
+-- Script to list all shows without a genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title;
