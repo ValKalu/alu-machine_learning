940b7dd842b25884110a3a219ea017e7ac4b1ab6
ValKalu <kalu.valentine01@gmail.com>
2024-07-05 17:07:55 +0200

    accending order lis

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -1,6 +1,6 @@
 -- Script that list all shows as tv_show,title-tv_show.genre_id
 SELECT tv_shows.title, tvshows_genres.genres_id
-FROM tv_shows
+FROM hbtn_0d_tv_shows
 LEFT JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title;
